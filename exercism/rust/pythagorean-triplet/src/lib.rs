4d03d131d1b6ad55beb1036fff2cd0502c1c1fef
Michal Franc <lam.michal.franc@gmail.com>
2018-08-03 22:41:09 +0100

    Brute force pythagorean triplet solution with mutation

--- exercism/rust/pythagorean-triplet/src/lib.rs
+++ exercism/rust/pythagorean-triplet/src/lib.rs
@@ -7,24 +7,20 @@
 // a^2 + b^2 = 1000000 - 2000(a+b) + a^2 + b^2 + 2ab
 // 2000(a+b) - 2ab = 1000000
 // 1000(a+b) - ab = 500000
+//
+const STOP: u32 = 1000;
+
 fn check(a: u32, b: u32) -> bool {
     return 1000 * (a + b) - a * b == 500000
 }
-
-pub fn find() -> Option<u32> {
-    return brute()
-}
-
 
 fn brute() -> Option<u32> {
 
     let mut a = 1;
     let mut b;
-    let stop = 1000;
-
-    while a != stop {
+    while a != STOP {
         b = 1;
-        while b != stop {
+        while b != STOP {
             if check(a, b) { 
                 println!("a: {}, b: {}, c: {}", a, b, (1000 - (a + b)));
                 return Some(a * b * (1000 - (a + b)))
@@ -35,4 +31,8 @@ fn brute() -> Option<u32> {
     }
 
     return None
+}
+
+pub fn find() -> Option<u32> {
+    return brute()
 }

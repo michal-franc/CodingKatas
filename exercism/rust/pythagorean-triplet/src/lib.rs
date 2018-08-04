699c2572c5a1937ed9bcc5b10ab8cf82ae940da9
Michal Franc <lam.michal.franc@gmail.com>
2018-08-04 12:50:20 +0100

    Applying menor suggestions removing mutability of a and b and replacing wiht more rust syntax of for in, Rust is also an expression language so return statements are not necessary

--- exercism/rust/pythagorean-triplet/src/lib.rs
+++ exercism/rust/pythagorean-triplet/src/lib.rs
@@ -11,26 +11,21 @@
 const STOP: u32 = 1000;
 
 fn check(a: u32, b: u32) -> bool {
-    return 1000 * (a + b) - a * b == 500000
+    1000 * (a + b) - a * b == 500000
 }
 
 fn brute() -> Option<u32> {
 
-    let mut a = 1;
-    let mut b;
-    while a != STOP {
-        b = 1;
-        while b != STOP {
+    for a in 1..STOP {
+        for b in 1..STOP {
             if check(a, b) { 
                 println!("a: {}, b: {}, c: {}", a, b, (1000 - (a + b)));
                 return Some(a * b * (1000 - (a + b)))
             }
-            b += 1;
         }
-        a += 1;
     }
 
-    return None
+    None
 }
 
 pub fn find() -> Option<u32> {

e8f226e37cc0b0ae2c80bc58242e8d3e4f0742e0
Michal Franc <lam.michal.franc@gmail.com>
2018-08-11 22:43:09 +0100

    in place rotation

--- cracking-coding-interviews/arrays/test_rotate_matrix.py
+++ cracking-coding-interviews/arrays/test_rotate_matrix.py
@@ -3,7 +3,7 @@ import unittest
 # Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4
 # bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
 
-def rotate(matrix):
+def rotate_not_in_place(matrix):
 
     l = len(matrix)
 
@@ -17,6 +17,25 @@ def rotate(matrix):
             result_matrix[abs(y - (l - 1))][x] = matrix[x][y]
 
     return result_matrix
+
+def rotate(matrix):
+
+    l = len(matrix)
+
+    if l <= 1:
+        return matrix
+
+    # for l -> 3 there is no second layer so it stays inside
+
+    for x in range(0, int(l/2)):
+        for y in range(x, l-x-1):
+            temp = matrix[x][y]
+            matrix[x][y] = matrix[y][l-1-x]
+            matrix[y][l-1-x] = matrix[l-1-x][l-1-y]
+            matrix[l-1-x][l-1-y] = matrix[l-1-y][x]
+            matrix[l-1-y][x] = temp
+
+    return matrix
 
 class SimpleTest(unittest.TestCase):
     def test_4x4(self):

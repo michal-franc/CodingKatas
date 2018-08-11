285c595bd5c649cfb69d011a87ce6c141064ecbd
Michal Franc <lam.michal.franc@gmail.com>
2018-08-11 22:26:21 +0100

    Rotate not in place

---
+++ cracking-coding-interviews/arrays/test_rotate_matrix.py
@@ -0,0 +1,60 @@
+import unittest
+
+# Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4
+# bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+
+def rotate(matrix):
+
+    l = len(matrix)
+
+    if l <= 1:
+        return matrix
+
+    result_matrix = [[''] * l for x in range(l)]
+
+    for y in range(l -1, -1, -1):
+        for x in range(l):
+            result_matrix[abs(y - (l - 1))][x] = matrix[x][y]
+
+    return result_matrix
+
+class SimpleTest(unittest.TestCase):
+    def test_4x4(self):
+        test_matrix = [['a', 'b', 'c', 'd'], ['e', 'f', 'g', 'h'], ['j', 'k', 'l', 'm'], ['o', 'p', 'r', 's']]
+        expected_result = [['d', 'h', 'm', 's'], ['c', 'g', 'l', 'r'], ['b', 'f', 'k', 'p'], ['a', 'e', 'j', 'o']]
+
+        actual_result = rotate(test_matrix)
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_3x3(self):
+        test_matrix = [['a', 'b'], ['c', 'd']]
+        expected_result = [['b', 'd'], ['a', 'c']]
+
+        actual_result = rotate(test_matrix)
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_2x2(self):
+        test_matrix = [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i']]
+        expected_result = [['c', 'f', 'i'], ['b', 'e', 'h'], ['a', 'd', 'g']]
+
+        actual_result = rotate(test_matrix)
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_empty(self):
+        test_matrix = []
+        expected_result = []
+
+        actual_result = rotate(test_matrix)
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_1by1(self):
+        test_matrix = ["a"]
+        expected_result = ["a"]
+
+        actual_result = rotate(test_matrix)
+
+        self.assertEqual(actual_result, expected_result)

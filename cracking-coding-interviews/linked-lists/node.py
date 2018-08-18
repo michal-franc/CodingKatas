8d90ddb09c5373b702b2d9041efd26207edb48f1
Michal Franc <lam.michal.franc@gmail.com>
2018-08-18 12:18:34 +0100

    Added helper function to visualise linked lists

--- cracking-coding-interviews/linked-lists/node.py
+++ cracking-coding-interviews/linked-lists/node.py
@@ -13,3 +13,41 @@ def node_len(start_node):
         len_counter += 1
 
     return len_counter
+
+def generate_next(arr,  n):
+
+    if len(arr) == 0:
+        n.next = None
+    else:
+        next_node = Node(arr[0], None)
+        n.next = next_node
+        generate_next(arr[1:], next_node)
+
+    return n
+
+def generate_node_list(arr):
+    start_node = Node(arr[0], None)
+    return generate_next(arr[1:], start_node)
+
+def generate_list_node(start_node):
+    buf = []
+
+    current_node = start_node
+
+    while current_node != None:
+        buf.append(current_node.val)
+        current_node = current_node.next
+
+    return buf
+
+def print_list(start_node):
+
+    buf = []
+
+    current_node = start_node
+
+    while current_node != None:
+        buf.append("%i -> " % current_node.val)
+        current_node = current_node.next
+
+    return ''.join(buf)

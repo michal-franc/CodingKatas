ad7562448f14572bf39725d629d06094eaf4ae56
Michal Franc <lam.michal.franc@gmail.com>
2018-10-21 14:19:36 +0100

    leet code divide two integers

---
+++ leetcode/29divide_two_integers.py
@@ -0,0 +1,70 @@
+# returns counter and reminder
+def count_divisions(dividend, divisor):
+
+    if divisor == 1:
+        return (dividend, 0)
+    if divisor == 2:
+        return (dividend >> 1, 0)
+    if divisor == dividend:
+        return (1, 0)
+
+    counter = 1
+    # base case is one we assume that number already can fit once
+    # 1 -> dividend < divisor
+    # so we avoid this case
+    # and move to the next one by shifting divisor to the left
+    # divisor << 1
+    while dividend > (divisor << 1):
+        divisor <<= 1
+        counter <<= 1
+
+    # next dividend is a reminder that doesnt fit so we return - dividend - divisor
+    return (counter, dividend - divisor)
+
+def divide_two_ints(dividend, divisor):
+
+    abs_dividend = abs(dividend)
+    abs_divisor = abs(divisor)
+
+    # 10 / 3
+    # 10 - 3 -> True (+1)
+    # 10 - (3*2) -> True (using shift) (+1)
+    # 10 - (3*2*2) -> False (using shift)
+    # 10 - (3*2) = 4 (4 is the reminder then we repeat the same process with shifting)
+
+    # Reminder 10 - 6 -> 4
+    # 4 / 3
+    # 4 - 3 -> True (+1)
+    # 3 - 6 -> False
+
+    # Reminder 4 - 3 -> 1
+    # check if reminder is smaller than divisor at first step if it is then stop
+    # 1 / 3 but 1 < 3 -> then stop
+
+    counter = 0
+    while abs_dividend >= abs_divisor:
+        (count , abs_dividend) = count_divisions(abs_dividend, abs_divisor)
+        counter += count
+
+    if dividend < 0:
+        counter = -counter
+
+    if divisor < 0:
+        counter = -counter
+
+    if counter >= (1 << 31):
+        return (1 << 31) - 1
+
+    return counter
+
+print(divide_two_ints(10, 5))
+print(divide_two_ints(6, 3))
+print(divide_two_ints(10, 3))
+print(divide_two_ints(7, -3))
+print(divide_two_ints(1, 1))
+print(divide_two_ints(-1, -1))
+print(divide_two_ints(-1, 1))
+print(divide_two_ints(-1123213233, 1))
+print(divide_two_ints(-1123213233, -1))
+print(divide_two_ints(-2147483648, -1))
+print(divide_two_ints(2147483647, 2))

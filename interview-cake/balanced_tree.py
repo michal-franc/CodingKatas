328d6bb81da47d2356cf6ff2b11a7238b35bd3e8
Michal Franc <lam.michal.franc@gmail.com>
2018-10-24 20:49:34 +0100

    Added more comments with explanation

--- interview-cake/balanced_tree.py
+++ interview-cake/balanced_tree.py
@@ -2,13 +2,21 @@ from binarytree import tree, bst, Node
 
 def is_tree_perfect(tree_root):
 
+    # if tree empty it is perfect
     if root is None:
         return True
 
+    # to store depths of 'leafs'
+    # expected at top 2 leafs as if
+    # more than 2 distint depths always assumes that there will be diff greater than 1
+    # example : [0, 1, 2] = 2 - 0 > 1
     depths = []
-    nodes = []
 
     # using 'stack' for dfs
+    # could use queue for bfs
+    nodes = []
+
+    # base case
     nodes.append((tree_root, 0))
 
     while len(nodes):
@@ -17,17 +25,18 @@ def is_tree_perfect(tree_root):
 
         # check if it is a leaf
         if node.left is None and node.right is None:
+            # check if unique depth (no need to store more depths)
+            # interested only on finding if tree is 'perfect'
             if depth not in depths:
                 depths.append(depth)
 
                 if len(depths) == 2:
                     return abs(depths[0] - depths[1]) <= 1
 
-                # more than 2 distint depths always assumes that there will be diff by 1
-                # example : [0, 1, 2] = 2 - 0 > 1
                 if len(depths) > 2:
                     return False
         else:
+            # traverse first left side then right side
             if node.left:
                 nodes.append((node.left, depth + 1))
             if node.right:
